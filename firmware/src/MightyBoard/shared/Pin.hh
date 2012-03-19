675085d9afaab0b171b4bf54cd441fbfd3aa39c6
Robert Giseburt <giseburt@gmail.com>
2012-03-19 00:47:34 -0500

    Restored Pin.

--- firmware/src/MightyBoard/shared/Pin.hh
+++ firmware/src/MightyBoard/shared/Pin.hh
@@ -4,8 +4,6 @@
 #include "AvrPort.hh"
 #include "Pin.hh"
 
-extern void ErrorDontUseBarePinDirectly();
-
 /// \ingroup HardwareLibraries
 class Pin {
 private:
@@ -16,8 +14,9 @@ private:
 	const uint8_t pin_mask;
 	const uint8_t pin_mask_inverted;
 
+	Pin() : port_base(NullPort.port_base), is_null(true), pin_index(0), pin_mask(0), pin_mask_inverted((uint8_t)~0) {}
+
 public:
-	Pin() : port_base(NullPort.port_base), is_null(true), pin_index(0), pin_mask(0), pin_mask_inverted((uint8_t)~0) {ErrorDontUseBarePinDirectly();}
 	Pin(const AvrPort& port_in, uint8_t pin_index_in) : port_base(port_in.port_base), is_null(port_base == NULL_PORT), pin_index(pin_index_in), pin_mask((uint8_t)_BV(pin_index_in)), pin_mask_inverted((uint8_t)~_BV(pin_index_in)) {}
 	
 	bool isNull() const { return is_null; }
