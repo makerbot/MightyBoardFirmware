9019ef30ec141554fab08add6f3f6af0633e3518
dgs3 <sayles.dave@gmail.com>
2012-12-04 11:48:41 -0500

    Modify how the SDMenu screen is drawn 	* We reverse the list so newer items are drawn first

--- dist/MightyBoard/load_firmware.sh
+++ dist/MightyBoard/load_firmware.sh
@@ -34,6 +34,7 @@ echo $BOOT8U2
 while true; do
 
 FAIL8U2="8U2 Bootloader PASS"
+FAILLOCK="1280 Lock Bits PASS"
 FAIL1280="1280 Bootloader PASS"
 FAILUSB="USB Program PASS"
 
@@ -46,6 +47,18 @@ FAILUSB="USB Program PASS"
     if [ $? -ne 0 ]
     then
      FAIL8U2="8U2 Bootloader FAIL"
+    fi
+
+   echo "Press to Check 1280 Lock Bits"
+    read
+
+    # Upload bootloader via isp
+    avrdude -p m1280 -F -P usb -c avrispmkii -U lock:w:0x0F:m
+
+    if [ $? -ne 0 ]
+     then
+      FAILLOCK="1280 Lock Bits FAIL"
+      echo FAILLOCK
     fi
 
    echo "Press Enter upload 1280 bootloader"
