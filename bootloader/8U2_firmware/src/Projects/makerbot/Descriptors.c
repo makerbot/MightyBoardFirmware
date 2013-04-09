72ce2e27cfa6703ef357c270a1df5152830c1d7a
Bill <wdc.rith@gmail.com>
2013-04-09 12:26:53 -0400

    Changing RevH PID to B016

--- bootloader/8U2_firmware/src/Projects/makerbot/Descriptors.c
+++ bootloader/8U2_firmware/src/Projects/makerbot/Descriptors.c
@@ -69,7 +69,7 @@ const USB_Descriptor_Device_t PROGMEM DeviceDescriptor =
 		
         .VendorID               = 0x23C1,
 
-        .ProductID          	= 0xB015,
+        .ProductID          	= 0xB016,
 	.ReleaseNumber          = 0x0001,
 		
 	.ManufacturerStrIndex   = 0x01,
