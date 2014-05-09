712b8328b581e5163d9a4728fc2babdea9b608aa
wdcrith <wdc.rith@gmail.com>
2014-05-09 15:38:18 -0400

    Messing with the PID loop values some more

--- firmware/src/MightyBoard/shared/Heater.hh
+++ firmware/src/MightyBoard/shared/Heater.hh
@@ -25,9 +25,9 @@
 #include "Types.hh"
 #include "Timeout.hh"
 
-#define DEFAULT_P 8.0
+#define DEFAULT_P 9.0
 #define DEFAULT_I 0.250
-#define DEFAULT_D 12.0
+#define DEFAULT_D 10.0
 
 enum HeaterFailMode{
 	HEATER_FAIL_NONE = 0,
